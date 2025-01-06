9d28c9cd58f5e9ed5adf83c3f21670b1a11f4ae7
Phnxss <fpsslab@gmail.com>
2025-01-06 14:11:48 -0500

    Update credentials.php

--- credentials.php
+++ credentials.php
@@ -1,4 +1,3 @@
 <?php
-$name = ""; // the name of your application
-$OwnerId = ""; // can be found in your account settings
-$SellerKey = ""; // application's seller key. Found in seller settings (You must have seller plan)
+$name = "Phnxauth"; // App name 
+$ownerid = "c7Y2u3kCiZ"; // Account ID 
